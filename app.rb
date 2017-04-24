6426c8fc9f1f284d1f2a704f845fe0ff0877db01
iwpeifer <iwpeifer@gmail.com>
2017-04-24 17:32:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do
+		response.write "My name is No"
+	end
+
+	get '/hometown' do
+		response.write "My hometown is No"
+	end
+
+	get '/favorite-song' do
+		response.write "My favorite song is No. You need to let it go. You need to let it go. You need to let it go."
+	end
+
 end
